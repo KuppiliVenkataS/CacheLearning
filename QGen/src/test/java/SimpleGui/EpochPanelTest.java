15c26c28e38e1470b39508f429099ea2bfc288c4
santhilatakv <santhilatakv@gmail.com>
2016-11-04 12:18:34 +0000

    Committing QGen

---
+++ QGen/src/test/java/SimpleGui/EpochPanelTest.java
@@ -0,0 +1,63 @@
+package SimpleGui;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by santhilata on 16/2/16.
+ */
+public class EpochPanelTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAddLabels() throws Exception {
+
+    }
+
+    @Test
+    public void testAddComponents() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDistributions() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNumbQs() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPercentQueryRepetition() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPercentSubqueryRepetition() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateUI() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUI() throws Exception {
+
+    }
+}
